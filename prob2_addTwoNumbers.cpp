cdcff10018b4ea1a1835543b67856770b77d13ea
Ganesh <gsw7517@gmail.com>
2024-08-27 10:22:57 +0530

    Solved 2nd question

--- prob2_addTwoNumbers.cpp
+++ prob2_addTwoNumbers.cpp
@@ -1,3 +1,38 @@
+/*
+1) ADD TWO NUMBERS
+
+You are given two non-empty linked lists representing two non-negative integers. The digits are stored in reverse order,
+and each of their nodes contains a single digit. Add the two numbers and return the sum as a linked list.
+You may assume the two numbers do not contain any leading zero, except the number 0 itself.
+
+Example 1:
+
+Input: l1 = [2,4,3], l2 = [5,6,4]
+Output: [7,0,8]
+Explanation: 342 + 465 = 807.
+
+Example 2:
+
+Input: l1 = [0], l2 = [0]
+Output: [0]
+
+Example 3:
+
+Input: l1 = [9,9,9,9,9,9,9], l2 = [9,9,9,9]
+Output: [8,9,9,9,0,0,0,1]
+
+ 
+
+Constraints:
+
+    The number of nodes in each linked list is in the range [1, 100].
+    0 <= Node.val <= 9
+    It is guaranteed that the list represents a number that does not have leading zeros.
+
+
+*/
+
+
 #include <iostream>
 using namespace std;
 
@@ -12,76 +47,58 @@ struct ListNode
 class Solution
 {
 public:
-    ListNode *addIt(string sum1, string sum2)
-    {
-
-        ListNode* head = new ListNode();
-        ListNode* temp = head;
-        int output =0, carry =0;
-        int os;
-        
-
-        for (int i = sum1.length()-1; i >=0 ; i--)
-        {
-
-            output = (((sum1[i] - '0') + (sum2[i] - '0') )+  carry);    
-            carry = output % 10;
-            os = (output%10)+'0';
-
-            if(head == nullptr){
-                head->val= os;
-            }else{
-                while(temp != nullptr){
-                    temp = temp->next;
-                }
-                ListNode *newNode = new ListNode(os);
-                temp->next = newNode;
-            }
-                 
-        }
-
-
-        return head;
-    
-    }
-
     ListNode *addTwoNumbers(ListNode *l1, ListNode *l2)
     {
 
-        string sum1, sum2, sum3;
+        int sum1 = 0,  carry =0;
+        ListNode* head = new ListNode(0);
+        ListNode* temp = head;
 
-        ListNode *temp1;
-        ListNode *temp2;
-        ListNode *output;
+        
+        while(l1 != nullptr || l2 != nullptr || carry != 0){
+        
 
-        temp1 = l1;
-        temp2 = l2;
+            if(l1 !=nullptr){
+                sum1 +=l1->val;
+                l1=l1->next;
+            }else{
+                sum1+=0;
+            }
+            
 
-        while (temp1 != nullptr)
-        {
-            char c = temp1->val + '0';
-            sum1 = c + sum1;
-            temp1 = temp1->next;
+            if(l2 != nullptr){
+                sum1 +=l2->val;
+                l2=l2->next;
+            }else{
+                sum1+=0;
+            }
+
+            sum1 += carry;
+            carry = sum1/10;
+            sum1 = sum1%10;
+
+            ListNode* newNode = new ListNode(sum1);         // 9999999
+                                                            // 9999
+            temp->next = newNode; 
+            temp = newNode;
+            cout<<sum1;
+
+            sum1 =0;
+
         }
 
-        while (temp2 != nullptr)
-        {
-            char c = temp2->val + '0';
-            sum2 = c + sum2;
-            temp2 = temp2->next;
-        }
-
-        output = addIt(sum1, sum2);
-
-        return output->next;
+        return head->next;
+     
     }
 };
+
 int main()
 {
-    ListNode *l1 = new ListNode(2, new ListNode(4, new ListNode(3)));
-    ListNode *l2 = new ListNode(5, new ListNode(6, new ListNode(4)));
+    ListNode *l1 = new ListNode(9, new ListNode(9, new ListNode(9 ,new ListNode(9 ,new ListNode(9 , new ListNode(9 , new ListNode(9 )) ) ))));
+    ListNode *l2 = new ListNode(9, new ListNode(9, new ListNode(9,new ListNode(9))));
 
     Solution s;
+
     s.addTwoNumbers(l1, l2);
 
     return 0;
