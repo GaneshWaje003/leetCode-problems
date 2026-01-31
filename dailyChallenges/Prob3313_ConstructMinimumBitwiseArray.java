c69252ff1a03fcec2c377e33634c90d4f9393697
Ganesh <gsw7517@gmail.com>
2026-01-31 11:22:00 +0530

    day 11

---
+++ dailyChallenges/Prob3313_ConstructMinimumBitwiseArray.java
@@ -0,0 +1,16 @@
+package dailyChallenges;
+
+import java.util.List;
+
+public class Prob3313_ConstructMinimumBitwiseArray {
+    public static void main(String[] args) {
+
+    }
+}
+
+class Solution {
+    public int[] minBitwiseArray(List<Integer> nums) {
+    
+
+    }
+}
